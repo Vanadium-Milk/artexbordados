b9b8c09377ab52cab716219fbad1b17f1acc9fb6
Kevin Torres <kevintu236@gmail.com>
2024-03-12 13:09:40 -0600

    Implementation of the first JavaScript functions.
    - I added the scrollable property of the color options in the Product Page.
    - I added some functions in the "test.js" document:
       * This functions allow to increment or reduce the quantity of products in the interfaces "Product Page" and "My Cart".

--- controllers/test.js
+++ controllers/test.js
@@ -1,1 +1,32 @@
+    var start = 1;
 
+    window.onload = function() {
+        update_price();
+        document.getElementById('quantity').textContent = start;
+    }
+
+    function increment(){
+        var quantityElement = document.getElementById('quantity');
+        var quantity = parseInt(quantityElement.textContent);
+        quantityElement.textContent = ++start;
+        var decrement_button = document.getElementById('less');
+        decrement_button.disabled = false;
+        update_price();
+    }
+
+    function decrease(){
+        var quantityElement = document.getElementById('quantity');
+        var quantity = parseInt(quantityElement.textContent);
+        quantityElement.textContent = --start;
+        var decrement_button = document.getElementById('less');
+        if (start == 0) {
+            decrement_button.disabled = true;
+        }
+        update_price();
+    }
+
+    function update_price(){
+        var item_price = 550;
+        var totalPrice = item_price * start;
+        document.getElementById('price').textContent = '$' + totalPrice;
+    }
